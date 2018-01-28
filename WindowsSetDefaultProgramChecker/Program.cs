515ef490b12767641815671f1d2a1179044397fa
tango1266 <tango1266@arcor.de>
2018-01-28 10:57:46 +0100

    removed unused references

--- WindowsSetDefaultProgramChecker/Program.cs
+++ WindowsSetDefaultProgramChecker/Program.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Input;
-using WindowsSetDefaultProgramChecker.ProcessList;
-using Newtonsoft.Json;
 
 namespace WindowsSetDefaultProgramChecker
 {
